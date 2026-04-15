d1c70a860203739474bcd55528f4e728da47423a
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-04-15 09:04:10 +0700

    bai thuc hanh so 2

--- quanlychitieu_manh_vinh_1_2026/lib/main.dart
+++ quanlychitieu_manh_vinh_1_2026/lib/main.dart
@@ -7,46 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Demo Quan li chi tieu',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Quan ly chi tieu'),
+      home: const MyHomePage(title: 'Quản lý chi tiêu'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,69 +28,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  //1. Biến
+  String userName = "Đỗ Xuân Vinh";
+  double totalMoney = 5000;
+  bool isSaving = true;
+
+  //2. Collection (chi tiêu)
+  final List<Map<String, dynamic>> expenses = [
+    {"name": "Ăn sáng", "amount": 30, "date": DateTime(2026, 4, 10)},
+    {"name": "Game", "amount": 50, "date": DateTime(2026, 4, 11)},
+    {"name": "Mua sắm", "amount": 100, "date": DateTime(2026, 4, 12)},
+  ];
+
+  //3. Collection (ví tiền)
+  final List<Map<String, dynamic>> wallets = [
+    {'id': 1, 'name': 'Ví tiền mặt'},
+    {'id': 2, 'name': 'Ngân hàng'},
+    {'id': 3, 'name': 'Momo'},
+  ];
+
+  // Tính tổng tiền đã chi
+  double get totalSpent {
+    double sum = 0;
+    for (var item in expenses) {
+      sum += item["amount"];
+    }
+    return sum;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Nguyen Duc Manh - 23010024'),
-            const Text('Do Xuan Vinh - 23017283'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            //4. Hiển thị biến
+            Text('User: $userName'),
+            Text('Tổng tiền: $totalMoney'),
+            Text('Đã chi: $totalSpent'),
+            Text(isSaving ? 'Đang tiết kiệm' : 'Tiêu quá tay'),
+
+            const SizedBox(height: 20),
+
+            //5. Hiển thị collection (expenses)
+            const Text('Danh sách chi tiêu:'),
+
+            // Header
+            Row(
+              children: const [
+                Expanded(flex: 3, child: Text('Tên')),
+                Expanded(flex: 2, child: Text('Tiền', textAlign: TextAlign.center)),
+                Expanded(flex: 2, child: Text('Ngày', textAlign: TextAlign.right)),
+              ],
+            ),
+
+            Column(
+              children: [
+                for (var item in expenses)
+                  Row(
+                    children: [
+                      Expanded(flex: 3, child: Text(item["name"])),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          '${item["amount"]}k',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          '${item["date"].day}/${item["date"].month}',
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+
+            //Hiển thị bằng map()
+            Column(
+              children: expenses.map((item) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Map: ${item["name"]}'),
+                    Text('${item["amount"]}k'),
+                  ],
+                );
+              }).toList(),
+            ),
+
+            const SizedBox(height: 20),
+
+            //7. Hiển thị collection (wallets)
+            const Text('Danh sách ví:'),
+
+            Column(
+              children: [
+                for (var w in wallets)
+                  Row(
+                    children: [
+                      Expanded(flex: 1, child: Text('${w["id"]}', )),
+                      Expanded(flex: 3, child: Text(w["name"], textAlign: TextAlign.right),),
+                    ],
+                  ),
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
