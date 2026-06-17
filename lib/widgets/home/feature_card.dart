5e135337b4c50ebe97a53a01b188d3828023e8f4
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-06-17 20:48:45 +0700

    refactor(feature-card): support dynamic styling and subtexts

--- lib/widgets/home/feature_card.dart
+++ lib/widgets/home/feature_card.dart
@@ -6,6 +6,7 @@ class FeatureCard extends StatelessWidget {
   final IconData icon;
   final Color backgroundColor;
   final Color iconColor;
+  final VoidCallback? onTap;
 
   const FeatureCard({
     super.key,
@@ -14,59 +15,65 @@ class FeatureCard extends StatelessWidget {
     required this.icon,
     required this.backgroundColor,
     required this.iconColor,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        height: 112,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(22),
+        child: Material(
           color: backgroundColor,
-          borderRadius: BorderRadius.circular(22),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              right: -4,
-              bottom: -4,
-              child: Opacity(
-                opacity: 0.15,
-                child: Icon(
-                  icon,
-                  size: 38,
-                  color: iconColor,
-                ),
+          child: InkWell(
+            onTap: onTap,
+            child: Container(
+              height: 112,
+              padding: const EdgeInsets.all(16),
+              child: Stack(
+                children: [
+                  Positioned(
+                    right: -4,
+                    bottom: -4,
+                    child: Opacity(
+                      opacity: 0.15,
+                      child: Icon(
+                        icon,
+                        size: 38,
+                        color: iconColor,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          color: Color(0xFF172033),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        subtitle,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          color: Color(0xFF26324A),
+                          fontSize: 13,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    color: Color(0xFF172033),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  subtitle,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    color: Color(0xFF26324A),
-                    fontSize: 13,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
